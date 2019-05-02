43e5120a1045f1ed87d96ede85dc55cfd842adcd
Lars Petersen <info@lars-petersen.net>
2019-05-03 00:19:33 +0200

    CPUID is executed only once now

--- lmp_amd64.h
+++ lmp_amd64.h
@@ -38,33 +38,46 @@ POSSIBILITY OF SUCH DAMAGE.
  * CPU feature detection
  *****************************************************************************/
 
-long lmp_cpu_has_popcnt()
-{
-    static long result;
+#define LMP_CPU_FEATURE_POPCNT (1UL << 23)
 
-    if (!result) {
-        __asm__ (
-            // Save clobbered registers on stack in order to not having to
-            // declare them which turned out to confuse the register allocator.
-            "pushq %%rbx;"
-            "pushq %%rcx;"
-            "pushq %%rdx;"
-            // Call CPUID function 1.
-            "mov $1, %%eax;"
-            "cpuid;"
-            "xor %%rax, %%rax;"
-            "bt $23, %%ecx;"
-            "adc $1, %0;"
-            // Restore registers.
-            "popq %%rdx;"
-            "popq %%rcx;"
-            "popq %%rbx;"
-            : "=a" (result)
-            :: "cc"
-        );
+static inline unsigned long lmp_cpu_get_features()
+{
+    unsigned long r;
+
+    __asm__ (
+        // Save clobbered registers on stack in order to not having to
+        // declare them which turned out to confuse the register allocator.
+        "pushq %%rbx;"
+        "pushq %%rcx;"
+        "pushq %%rdx;"
+        "xor %%rcx, %%rcx;"
+        "xor %%rdx, %%rdx;"
+        // Call CPUID function 1.
+        "mov $1, %%eax;"
+        "cpuid;"
+        "mov %%edx, %%eax;"
+        "shl $32, %%rax;"
+        "xor %%rcx, %%rax;"
+        // Restore registers.
+        "popq %%rdx;"
+        "popq %%rcx;"
+        "popq %%rbx;"
+        : "=a" (r)
+        :: "cc"
+    );
+
+    return r;
+}
+
+static inline unsigned long lmp_cpu_has_feature(unsigned long feature)
+{
+    static unsigned long lmp_cpu_features;
+
+    if (!lmp_cpu_features) {
+        lmp_cpu_features = lmp_cpu_get_features();
     }
 
-    return result - 1;
+    return lmp_cpu_features & feature;
 }
 
 /*****************************************************************************
@@ -142,7 +155,7 @@ size_t lmp_popcount(
 {
     size_t r = 0;
 
-    if (lmp_cpu_has_popcnt()) {
+    if (lmp_cpu_has_feature(LMP_CPU_FEATURE_POPCNT)) {
         for (; an > 0; an--) {
             r += lmp_popcount_1(ap[an - 1]);
         }
