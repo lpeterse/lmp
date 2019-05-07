39038886e016bec9fb76fcaa05f7560c8189e9d6
Lars Petersen <info@lars-petersen.net>
2019-05-07 22:59:17 +0200

    Fixed over-optimization bug in lmp_sub_mmb

--- src/amd64/lmp_sub_mmb.h
+++ src/amd64/lmp_sub_mmb.h
@@ -6,48 +6,48 @@ inline lmp_limb_t lmp_sub_mmb(
 {
     lmp_limb_t res;
 
-    __asm__ (
+    __asm__ volatile (
         // Immediate return if m == 0
-        "movq    %[borrow], %[res];"
-        "jrcxz   4f;"
+        "movq    %[borrow], %[res];\n\t"
+        "jrcxz   4f;\n\t"
         // Address increment shall not interfere with carry flag
-        "leaq    (%[rp],%[m],8), %[rp];"
-        "leaq    (%[ap],%[m],8), %[ap];"
-        "leaq    (%[bp],%[m],8), %[bp];"
-        "negq    %[m];"
+        "leaq    (%[rp],%[m],8), %[rp];\n\t"
+        "leaq    (%[ap],%[m],8), %[ap];\n\t"
+        "leaq    (%[bp],%[m],8), %[bp];\n\t"
+        "negq    %[m];\n\t"
         // Skip initial subtraction if m is even
-        "bt      $0, %[m];"
-        "jnc     1f;"
+        "bt      $0, %[m];\n\t"
+        "jnc     1f;\n\t"
         // Do one subtraction in advance if m is odd
-        "bt      $0, %[borrow];"
-        "movq    (%[ap],%[m],8), %[borrow];"
-        "sbbq    (%[bp],%[m],8), %[borrow];"
-        "movq    %[borrow], (%[rp],%[m],8);"
-        "leaq    1(%[m]), %[m];"
-        "jrcxz   3f;"
-        "jmp     2f;"
-    "1:;"
+        "bt      $0, %[res];\n\t"
+        "movq    (%[ap],%[m],8), %[res];\n\t"
+        "sbbq    (%[bp],%[m],8), %[res];\n\t"
+        "movq    %[res], (%[rp],%[m],8);\n\t"
+        "leaq    1(%[m]), %[m];\n\t"
+        "jrcxz   3f;\n\t"
+        "jmp     2f;\n"
+    "1:;\n\t"
         // Initialize carry flag
-        "bt      $0, %[borrow];"
-    "2:;"
+        "bt      $0, %[res];\n"
+    "2:;\n\t"
         // Do two subtractions per loop iteration
-        "movq    (%[ap],%[m],8), %[borrow];"
-        "sbbq    (%[bp],%[m],8), %[borrow];"
-        "movq    %[borrow], (%[rp],%[m],8);"
-        "movq    8(%[ap],%[m],8), %[borrow];"
-        "sbbq    8(%[bp],%[m],8), %[borrow];"
-        "movq    %[borrow], 8(%[rp],%[m],8);"
-        "leaq    2(%[m]), %[m];"
-        "jrcxz   3f;"
-        "jmp     2b;"
-    "3:;"
+        "movq    (%[ap],%[m],8), %[res];\n\t"
+        "sbbq    (%[bp],%[m],8), %[res];\n\t"
+        "movq    %[res], (%[rp],%[m],8);\n\t"
+        "movq    8(%[ap],%[m],8), %[res];\n\t"
+        "sbbq    8(%[bp],%[m],8), %[res];\n\t"
+        "movq    %[res], 8(%[rp],%[m],8);\n\t"
+        "leaq    2(%[m]), %[m];\n\t"
+        "jrcxz   3f;\n\t"
+        "jmp     2b;\n"
+    "3:;\n\t"
         // Store carry flag as final borrow
-        "movl    $0, %k[res];"
-        "setb    %b[res];"
-    "4:;"
-        : [rp] "+&r" (rp), [ap] "+&r" (ap), [bp] "+&r" (bp),
-          [m] "+&c" (m), [borrow] "+&r" (borrow), [res] "=&a" (res)
-        :
+        "movq    $0, %[res];\n\t"
+        "setb    %b[res];\n"
+    "4:;\n\t"
+        : [rp] "+r" (rp), [ap] "+r" (ap), [bp] "+r" (bp),
+          [m] "+c" (m), [res] "=r" (res)
+        : [borrow] "rmi" (borrow)
         : "cc", "memory"
     );
 
