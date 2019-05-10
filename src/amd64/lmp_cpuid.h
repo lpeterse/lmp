d41052f6565aa4c436dc36e2d5947a0be75eaf1a
Lars Petersen <info@lars-petersen.net>
2019-05-10 17:45:02 +0200

    Adds CPUID feature test for ADX

--- src/amd64/lmp_cpuid.h
+++ src/amd64/lmp_cpuid.h
@@ -1,27 +1,39 @@
 #define LMP_CPU_FEATURE_POPCNT (1UL << 23)
+#define LMP_CPU_FEATURE_ADX    (1UL << 19)
 
-static inline unsigned long lmp_cpu_get_features()
+static unsigned long lmp_cpu_get_features()
 {
     unsigned long r;
 
-    __asm__ (
+    __asm__ volatile (
         // Save clobbered registers on stack in order to not having to
         // declare them which turned out to confuse the register allocator.
+        "pushq   %%r8;"
         "pushq   %%rbx;"
         "pushq   %%rcx;"
         "pushq   %%rdx;"
-        "xor     %%rcx, %%rcx;"
-        "xor     %%rdx, %%rdx;"
+        // Set the lowest bit in order to mark cpuid as executed.
+        "movq    $1, %%r8;"
         // Call CPUID function 1.
         "mov     $1, %%eax;"
+        "xor     %%rcx, %%rcx;"
+        "xor     %%rdx, %%rdx;"
         "cpuid;"
-        "mov     %%edx, %%eax;"
-        "shl     $32, %%rax;"
-        "xor     %%rcx, %%rax;"
+        "and     $8388608, %%rcx;" // Filter for POPCNT extension flag
+        "or      %%rcx, %%r8;"
+        // Call CPUID function 7.
+        "mov     $7, %%eax;"
+        "xor     %%rcx, %%rcx;"
+        "xor     %%rdx, %%rdx;"
+        "cpuid;"
+        "and     $524288, %%rbx;" // Filter for ADX extension flag
+        "or      %%rbx, %%r8;"
         // Restore registers.
+        "movq    %%r8, %%rax;"
         "popq    %%rdx;"
         "popq    %%rcx;"
         "popq    %%rbx;"
+        "popq    %%r8;"
         : "=a" (r)
         :: "cc"
     );
--- src/amd64/lmp_mul_m1_add.h
+++ src/amd64/lmp_mul_m1_add.h
@@ -1,5 +1,5 @@
 #define LMP_MUL_M1_ADD
-inline lmp_limb_t lmp_mul_m1_add(
+static inline lmp_limb_t lmp_mul_m1_add_adx(
           lmp_limb_t *restrict rp,
     const lmp_limb_t *restrict ap, size_t m, lmp_limb_t b)
 {
@@ -109,4 +109,101 @@ inline lmp_limb_t lmp_mul_m1_add(
     );
 
     return hi2;
+}
+
+static inline lmp_limb_t lmp_mul_m1_add_noadx(
+          lmp_limb_t *restrict rp,
+    const lmp_limb_t *restrict ap, size_t m, lmp_limb_t b)
+{
+    lmp_limb_t rax;
+    lmp_limb_t rdx;
+    lmp_limb_t carry = 0;
+
+    __asm__ (
+        "leaq    (%[rp],%[m],8), %[rp];"
+        "leaq    (%[ap],%[m],8), %[ap];"
+        "negq    %[m];"
+        "jz      4f;"
+    "1:;"
+        "bt      $0, %[m];"
+        "jnc     2f;"
+        "movq    %[b], %[rax];"
+        "mulq    (%[ap],%[m],8);"
+        "addq    %[carry], %[rax];"
+        "adcq    $0, %[rdx];"
+        "addq    %[rax], (%[rp],%[m],8);"
+        "adcq    $0, %[rdx];"
+        "movq    %[rdx], %[carry];"
+        "add     $1, %[m];"
+        "jz      4f;"
+    "2:;"
+        "bt      $1, %[m];"
+        "jnc     3f;"
+        "movq    %[b], %[rax];"
+        "mulq    (%[ap],%[m],8);"
+        "addq    %[carry], %[rax];"
+        "adcq    $0, %[rdx];"
+        "addq    %[rax], (%[rp],%[m],8);"
+        "adcq    $0, %[rdx];"
+        "movq    %[rdx], %[carry];"
+        "movq    %[b], %[rax];"
+        "mulq    8(%[ap],%[m],8);"
+        "addq    %[carry], %[rax];"
+        "adcq    $0, %[rdx];"
+        "addq    %[rax], 8(%[rp],%[m],8);"
+        "adcq    $0, %[rdx];"
+        "movq    %[rdx], %[carry];"
+        "add     $2, %[m];"
+        "jz      4f;"
+    "3:;"
+        ".p2align 4, 0x90;"
+        "movq    %[b], %[rax];"
+        "mulq    (%[ap],%[m],8);"
+        "addq    %[carry], %[rax];"
+        "adcq    $0, %[rdx];"
+        "addq    %[rax], (%[rp],%[m],8);"
+        "adcq    $0, %[rdx];"
+        "movq    %[rdx], %[carry];"
+        "movq    %[b], %[rax];"
+        "mulq    8(%[ap],%[m],8);"
+        "addq    %[carry], %[rax];"
+        "adcq    $0, %[rdx];"
+        "addq    %[rax], 8(%[rp],%[m],8);"
+        "adcq    $0, %[rdx];"
+        "movq    %[rdx], %[carry];"
+        "movq    %[b], %[rax];"
+        "mulq    16(%[ap],%[m],8);"
+        "addq    %[carry], %[rax];"
+        "adcq    $0, %[rdx];"
+        "addq    %[rax], 16(%[rp],%[m],8);"
+        "adcq    $0, %[rdx];"
+        "movq    %[rdx], %[carry];"
+        "movq    %[b], %[rax];"
+        "mulq    24(%[ap],%[m],8);"
+        "addq    %[carry], %[rax];"
+        "adcq    $0, %[rdx];"
+        "addq    %[rax], 24(%[rp],%[m],8);"
+        "adcq    $0, %[rdx];"
+        "movq    %[rdx], %[carry];"
+        "add     $4, %[m];"
+        "jnz     3b;"
+    "4:;"
+        : [rax] "=&a" (rax), [rdx] "=&d" (rdx), [m] "+&r" (m), [carry] "+&r" (carry)
+        , [rp] "+&r" (rp), [ap] "+&r" (ap)
+        : [b] "r" (b)
+        : "cc", "memory"
+    );
+
+    return carry;
+}
+
+inline lmp_limb_t lmp_mul_m1_add(
+          lmp_limb_t *restrict rp,
+    const lmp_limb_t *restrict ap, size_t m, lmp_limb_t b)
+{   
+    if (lmp_cpu_has_feature(LMP_CPU_FEATURE_ADX)) {
+        return lmp_mul_m1_add_adx(rp, ap, m, b);
+    } else {
+        return lmp_mul_m1_add_noadx(rp, ap, m, b);
+    }
 }
